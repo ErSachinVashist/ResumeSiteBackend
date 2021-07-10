3bdf3b97e261d3345b5b0de3d96fa8e668d61e15
ErSachinVashist <sachinvashist82@gmail.com>
2021-07-11 02:16:30 +0530

    Changed DB

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports ={
-    mongoUrl:'mongodb+srv://'+process.env.MONGOUSER+':'+process.env.MONGOPASS+'@sachincluster.ekgy1.mongodb.net/myresumesite?retryWrites=true&w=majority',
-    dbName:'myresumesite'
+    mongoUrl:'mongodb+srv://'+process.env.MONGOUSER+':'+process.env.MONGOPASS+'@sachincluster.ekgy1.mongodb.net/'+process.env.MONGODB+'?retryWrites=true&w=majority',
+    dbName:process.env.MONGODB
 }
